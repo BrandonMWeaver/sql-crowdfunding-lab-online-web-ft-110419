406a6346cc70086c4fb38c1a73d9b5bd2d735cb1
Brandon Weaver <a7xncob@gmail.com>
2019-12-10 16:34:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -14,8 +14,8 @@ end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
   "SELECT projects.title, SUM(pledges.amount) - projects.funding_goal AS amount_over FROM projects
-   WHERE amount_over > 0
    JOIN pledges ON projects.id = pledges.project_id
+   WHERE amount_over > 0
    GROUP BY projects.id;"
 end
 
